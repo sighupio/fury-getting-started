86195a6e1fcd4fe3c2d1693ef9dcb75ec49994f8
Olivier BEAUTIER <raclepoulpe@gmail.com>
2022-11-03 08:43:33 +0000

    fix output names

--- fury-on-ovhcloud/infrastructure/network/output.tf
+++ fury-on-ovhcloud/infrastructure/network/output.tf
@@ -6,11 +6,11 @@ output "public_ip" {
   value = var.IP
 }
 
-output "pvnw_apps" {
+output "myPrivateNetwork" {
   value = tolist(ovh_cloud_project_network_private.myPrivateNetwork.regions_attributes)[0].openstackid
 }
 
-output "subnet_apps" {
+output "mySubnet" {
   value = openstack_networking_subnet_v2.mySubnet.id
 }
 
