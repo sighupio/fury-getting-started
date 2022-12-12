8aab49e3b003603ab9feddfd4bd8bc5d2821fab8
Samuele Chiocca <samuele@sighup.io>
2022-12-12 16:05:08 +0100

    feat: update terraform project for eks 1.24

--- fury-on-eks/terraform/outputs.tf
+++ fury-on-eks/terraform/outputs.tf
@@ -1,7 +1,13 @@
 output "velero_patch" {
-  description = "Velero Kustomize patches"
-  value       = module.velero.kubernetes_patches
+  description = "Velero Kustomize patches for deployment"
+  value       = module.velero.deployment
   sensitive   = true
+}
+
+output "velero_service_account_patch" {
+  description = "Velero Service Account patches"
+  value = module.velero.service_account
+  sensitive = true
 }
 
 output "velero_backup_storage_location" {
@@ -14,4 +20,10 @@ output "velero_volume_snapshot_location" {
   description = "Velero VolumeSnapshotLocation CRD"
   value       = module.velero.volume_snapshot_location
   sensitive   = true
+}
+
+output "ebs_csi_driver_patches" {
+  description = "EBS CSI Driver Kustomize patches"
+  value = module.ebs_csi_driver_iam_role.ebs_csi_driver_patches
+  sensitive = true
 }
